5dccaf677b1097a6ecdc2863c7af85051b4c3b14
Mitali-Juvekar <mitalijuvekar@gmail.com>
2024-09-08 10:41:46 -0400

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+
+# Create your views here.
+
+@csrf_exempt
+def home(request):
+    return HttpResponse("Welcome to the Dashboard!")
+
+@csrf_exempt
+def candlestick_data(request):
+    data = {
+        "data": [
+            {"x": "2023-01-01", "open": 30, "high": 40, "low": 25, "close": 35},
+            {"x": "2023-01-02", "open": 35, "high": 45, "low": 30, "close": 40},
+        ]
+    }
+    return JsonResponse(data)
+
+@csrf_exempt
+def line_chart_data(request):
+    data = {
+        "labels": ["Jan", "Feb", "Mar", "Apr"],
+        "data": [10, 20, 30, 40]
+    }
+    return JsonResponse(data)
+
+@csrf_exempt
+def bar_chart_data(request):
+    data = {
+        "labels": ["Product A", "Product B", "Product C"],
+        "data": [100, 150, 200]
+    }
+    return JsonResponse(data)
+
+@csrf_exempt
+def pie_chart_data(request):
+    data = {
+        "labels": ["Red", "Blue", "Yellow"],
+        "data": [300, 50, 100]
+    }
+    return JsonResponse(data)
